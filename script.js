d6f8a127995433fa9ca721bead6079679f4a9059
Navya <88023981+navysai1419@users.noreply.github.com>
2024-02-12 17:02:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 //complete this code
-class Animal {}
+class Animal {
 
-class Dog extends Animal {}
+	makeSound(){
+		
+	}
+	
+}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+	function purr(){
+		cosole.log('purr')
+	}
+	
+}
 
+class Cat extends Animal {
+function bark(){
+	consile.log('woof')
+}
+	
+}
+
+
+
+
+const myCat = new Cat("Siamese");
+myCat.makeSound(); 
+myCat.purr();
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
